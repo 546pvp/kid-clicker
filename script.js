6639cf02f663c147baaa56c70c55b5ea07b74d88
546pvp <79662987+546pvp@users.noreply.github.com>
2023-11-20 10:07:15 +0000

    major

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
   var clicks = 0
+  var clicksToAdd = 1
   var scoreHeader = document.querySelector("#scoreHeader")
   var button = document.querySelector("#button")
+  var buykidbtn1 = document.querySelector("#buykidbtn")
   button.addEventListener("click",()=>{
-    clicks++
+    clicks += clicksToAdd
     scoreHeader.textContent = clicks + " €"
-    if(clicks > 30){
-      
-    }
   })
